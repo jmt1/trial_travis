024c9802f15bf0ea0bbc9d3c7fdd3c5322f19042
jmt <javier.martinez@embention.com>
2019-02-04 12:46:22 +0100

    new scrip

--- jen.py
+++ jen.py
@@ -6,20 +6,4 @@ from pkg_resources import resource_string
 from jenkinsapi.jenkins import Jenkins
 
 jenkins = Jenkins('http://jenkins.embention.net:1136/')
-job_name = 'exampletravis'
-xml = resource_string('examples', 'addjob.xml')
-
-print(xml)
-
-job = jenkins.create_job(jobname=job_name, xml=xml)
-
-# Get job from Jenkins by job name
-my_job = jenkins[job_name]
-print(my_job)
-
-# Delete job using method in Jenkins class
-#
-# Another way is to use:
-#
-# del jenkins[job_name]
-jenkins.delete_job(job_name)
+print(jenkins.version)

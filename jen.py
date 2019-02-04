c587adb8883fa259a1f1fd139e37ba0c83ec08c2
jmt <javier.martinez@embention.com>
2019-02-04 14:35:33 +0100

    solve execution

--- jen.py
+++ jen.py
@@ -1,24 +1,31 @@
 from __future__ import print_function
 
 from jenkinsapi.jenkins import Jenkins
+from jenkinsapi.utils.crumb_requester import CrumbRequester
+host="http://192.168.0.19:1136/"
+jenkins = Jenkins(host,   requester=CrumbRequester(baseurl=host))
 
-jenkins = Jenkins('http://jenkins.embention.net:1136/')
-
-params = {}
+params = {'VERSION': '1.2.3', 'PYTHON_VER': '2.7'}
 job='exampletravis'
 
 # This will start the job in non-blocking manner
-jenkins.build_job(job)
+#jenkins.build_job(job)
 
 
 # This will start the job and will return a QueueItem object which
 # can be used to get build results
 job = jenkins[job]
-qi = job.invoke()
+qi = job.invoke(build_params=params)
 
 # Block this script until build is finished
 if qi.is_queued() or qi.is_running():
     qi.block_until_complete()
 
 build = qi.get_build()
+data=int(str(build).split("#")[1])
+last_failed = job.get_last_failed_buildnumber()
+if data==last_failed:
+    print("Ha falladooo esta build: "+str(build))
+    exit(1)
+print("Todo Correcto")
 print(build)

8e2ec3487001751bddccf5fb5b4799e3e1135ee8
jmt <javier.martinez@embention.com>
2019-02-06 00:28:11 +0100

    cc

--- scripts/comment.sh
+++ scripts/comment.sh
@@ -1,3 +1,1 @@
-curl -H "Authorization: token 256203e7cc261e9343eeeeeef47129b93d096233" -X POST \
--d "{\"body\": \"Hello world\"}" \
-"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+curl -H "Authorization: token 256203e7cc261e9343eeeeeef47129b93d096233" -X POST \ -d "{\"body\": \"Hello world\"}" \ "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
